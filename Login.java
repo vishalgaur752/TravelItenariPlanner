7d67a2d0a5152c12bbca2e31ccb27ac941ef6917
vishalgaur752 <vishalgaur752@gmail.com>
2024-06-11 19:30:06 +0530

    Java_Project

--- Login.java
+++ Login.java
@@ -1,5 +1,36 @@
-public class Login {
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+    Login() {
+        setSize(900, 350);
+        setLocation(320, 200);
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(146, 199, 207));
+        p1.setBounds(0, 0, 400, 400);
+        p1.setLayout(null);
+        add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.png"));
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(100, 100, 200, 200);
+        p1.add(image);
+
+        JPanel p2 = new JPanel();
+        p2.setLayout(null);
+        p2.setBounds(400, 30, 450, 250);
+        add(p2);
+
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        
+        Login l = new Login();
     }
 }
